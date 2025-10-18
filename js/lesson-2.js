35c127291fcf93a20c5c0a06ced37ef62a14e39f
Nataliia Kalinina <kalininanatasha@gmail.com>
2025-10-18 08:07:36 +0200

    lesson 2

--- js/lesson-2.js
+++ js/lesson-2.js
@@ -187,3 +187,41 @@ function findLongestWord(string) {
 }
 
 console.log(findLongestWord("London is the capital of Great Britain"));
+
+// Напишіть скрипт, який для об'єкту user, послідовно:
+// 1 - додасть поле mood зі значенням 'happy',
+// 2 - замінить hobby на 'skydiving',
+// 3 - замінить значення premium на false,
+// 4 - виведе зміст об'єкта user у форматі
+// '<ключ>:<значення>' використовуя Object.keys() та for...of
+
+// const user = {
+//     name: "John",
+//     age: 20,
+//     hobby: "tenis",
+//     premium: true,
+//   };
+
+const user = {
+  name: "John",
+  age: 20,
+  hobby: "tenis",
+  premium: true,
+};
+
+user.mood = "happy";
+user.hobby = "skydiving";
+user.premium = !true;
+
+function getData(user) {
+  const result = [];
+  const keys = Object.keys(user);
+  for (const key of keys) {
+    result.push(`${key}: ${user[key]`});
+    }
+    return ${ key }:${ value };
+}
+
+console.log(user);
+
+console.log(getData(user));
